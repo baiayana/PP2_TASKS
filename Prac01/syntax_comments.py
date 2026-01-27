f672c0133e88686eafd3c89083f4a440412ef9f1
baiayana <baigadilova.ayana@gmail.com>
2026-01-27 11:42:11 +0500

    prac01 in progress

---
+++ Prac01/syntax_comments.py
@@ -0,0 +1,22 @@
+#Syntax and comments                                                    #1
+'''>>> print("Hello, World!")
+Hello, World!'''
+#2
+#C:\Users\Your Name>python myfile.py
+#3
+#This is a comment
+print("Hello, World!")
+#4
+"""
+This is a comment
+written in
+more than just one line
+"""
+print("Hello, World!")
+#5
+print("Hello World!")
+print("Have a good day.")
+print("Learning Python is fun!")
+#6
+#rare and hard to read
+print("Hello"); print("How are you?"); print("Bye bye!")
