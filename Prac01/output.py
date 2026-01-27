aa91f25071dd2cbee5cbe26dbe4eb91c9908be92
baiayana <baigadilova.ayana@gmail.com>
2026-01-27 12:03:12 +0500

    added topic output

---
+++ Prac01/output.py
@@ -0,0 +1,20 @@
+#output 
+#1
+print("This will work!")
+print('This will also work!')
+#2
+#to print on the same line
+print("Hello World!", end=" ")
+print("I will print on the same line.")
+#3
+#no quotes for numbers
+print(3)
+print(358)
+print(50000)
+#4
+#math inside print
+print(3 + 3)
+print(2 * 5)
+#5
+#mix text and numbers using comma
+print("I am", 35, "years old.")
