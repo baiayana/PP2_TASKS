a48418a27111f8ca4fbc0c3d41841c47b2ba438c
baiayana <baigadilova.ayana@gmail.com>
2026-02-01 20:56:27 +0500

    Add Practice2 if_else

---
+++ Prac02/if_else.py
@@ -0,0 +1,44 @@
+#1
+a = 200
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+else:
+  print("a is greater than b")
+
+#2
+a = 10
+b = 20
+bigger = a if a > b else b
+print("Bigger is", bigger)
+
+#3
+a = 330
+b = 330
+print("A") if a > b else print("=") if a == b else print("B")
+
+#4
+age = 25
+is_student = False
+has_discount_code = True
+
+if (age < 18 or age > 65) and not is_student or has_discount_code:
+  print("Discount applies!")
+
+#5
+score = 85
+attendance = 90
+submitted = True
+
+if score >= 60:
+  if attendance >= 80:
+    if submitted:
+      print("Pass with good standing")
+    else:
+      print("Pass but missing assignment")
+  else:
+    print("Pass but low attendance")
+else:
+  print("Fail")
