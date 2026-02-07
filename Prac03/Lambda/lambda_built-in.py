0996aa5af0beca1ef7fd31b25c985a25266b2d1a
baiayana <baigadilova.ayana@gmail.com>
2026-02-07 17:52:24 +0500

    Add Practice3 built-in_functions

---
+++ Prac03/Lambda/lambda_built-in.py
@@ -0,0 +1,23 @@
+#1
+#map
+numbers = [1, 2, 3, 4, 5]
+doubled = list(map(lambda x: x * 2, numbers))
+print(doubled)
+
+#2
+#filter
+numbers = [1, 2, 3, 4, 5, 6, 7, 8]
+odd_numbers = list(filter(lambda x: x % 2 != 0, numbers))
+print(odd_numbers)
+
+#3
+#sorted
+students = [("Emil", 25), ("Tobias", 22), ("Linus", 28)]
+sorted_students = sorted(students, key=lambda x: x[1])
+print(sorted_students)
+
+#4
+#Sort strings by length
+words = ["apple", "pie", "banana", "cherry"]
+sorted_words = sorted(words, key=lambda x: len(x))
+print(sorted_words)
