05117a6e50b0ccb618fd69bd63d239b284992168
baiayana <baigadilova.ayana@gmail.com>
2026-02-07 18:29:49 +0500

    Add Practice3 object_properties

---
+++ Prac03/Classes_and_Objects/object_properties.py
@@ -0,0 +1,49 @@
+#1
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+p1 = Person("Emil", 36)
+
+print(p1.name)
+print(p1.age)
+
+#2
+#Access the properties of an object
+class Car:
+  def __init__(self, brand, model):
+    self.brand = brand
+    self.model = model
+
+car1 = Car("Toyota", "Corolla")
+
+print(car1.brand)
+print(car1.model)
+
+#3
+#You can modify the value of properties on objects
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+p1 = Person("Tobias", 25)
+print(p1.age)
+
+p1.age = 26
+print(p1.age)
+
+#4
+#You can delete properties from objects using the del keyword
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+p1 = Person("Linus", 30)
+
+del p1.age
+
+print(p1.name) # This works
+# print(p1.age) # This would cause an error
